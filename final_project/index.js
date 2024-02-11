8f2df9ca0b530c2fc5c8a83a5f6064bd993e3640
egegurel <ege_gurel35@hotmail.com>
2024-02-11 06:15:31 +0300

    first commit

--- final_project/index.js
+++ final_project/index.js
@@ -10,11 +10,43 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+app.use("/customer/auth/*", async function auth(req, res, next) {
+  const tokenz = req.headers.authorization;
+  let token;
+  if (tokenz.startsWith('Bearer ')) {
+   token = tokenz.slice(7);}
+
+  console.log('qqqqqqqqqq',token);
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided.' });
+  }
+
+  const secret = "fingerprint_customer";
+  console.log('token',token);
+  console.log('secret',secret);
+  console.log(token==secret);
+  try {
+      await verifyToken(token, secret);
+   //   req.user = userData;
+     next();
+  } catch (error) {
+   // console.log(error);
+      return res.status(403).json({ message: 'Failed to authenticate token.' });
+  }
 });
- 
-const PORT =5000;
+
+function verifyToken(token, secret) {
+  try {
+    const decoded = jwt.verify(token, secret);
+    console.log('Decoded payload:', decoded);
+  } catch (err) {
+    console.error("JWT Verification Error:", err.message);
+  }
+
+}
+
+const PORT = 5003;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
